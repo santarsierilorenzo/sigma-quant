20e90560e7f83bcd3f8ca5eb840f1a2f548ea369
santarsierilorenzo <lorenzo.santarsieri@outlook.com>
2026-02-08 14:24:24 +0000

    sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,13 +32,13 @@ exclude_patterns: List[str] = [
 
 source_suffix = ".rst"
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_book_theme"
 
 html_theme_options = {
-    "show_toc_level": 2,
-    "navigation_depth": 2,
-    "collapse_navigation": False,
-    "show_nav_level": 2,
+    "repository_url": "",
+    "use_edit_page_button": False,
+    "use_repository_button": False,
+    "home_page_in_toc": True,
 }
 
 html_static_path = ["_static"]
